9ce114f45f6ad7859750493a61229aebfb622e35
YoungseopLee <73778056+YoungseopLee@users.noreply.github.com>
2024-02-28 00:05:15 +0900

    dev 브랜치 생성 - controller, service, entity 등 패키지 생성 - HomeController 추가 및 기본적인 Home.html 띄우기

---
+++ src/main/java/com/example/yggdrasil/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.yggdrasil.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping("/Yggdrasil")
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String defaultPage() {
+        return "redirect:/Home";
+    }
+
+    @GetMapping("/Home")
+    public String home() {
+        log.info("HomeController 테스트 home() called");
+        return "home";
+    }
+
+
+
+}
